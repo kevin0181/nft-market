a15857a4224033b161e9406c2e9664bb8cbc5fa5
kevin <kevin0181@naver.com>
2022-08-08 16:33:03 +0900

    변수명 초뭏ㄷ

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Caver from "caver-js";
 
 //컨트렉트 주소 0x8983dA5d7f24Cb0314332c038eC6Ee8c1DFF552b (Count.sol)
 
-const COUNT_COUNTRACT_ADRESS = '0x1e8c426B58F6c26BD9A9Bf4E988F7371B87af110'; //contract address
+const COUNT_CONTRACT_ADDRESS = '0x1e8c426B58F6c26BD9A9Bf4E988F7371B87af110'; //contract address
 const ACCESS_KEY_ID = "KASKKBBA9XK2CNVHJRXNIVBY" //kas access key
 const SECRET_ACCESS_KEY = "q72Y3eh-qIcu-YmylbIvIOu27VNmJzFXgduUg2eJ"; //kas secret access key
 
@@ -53,7 +53,7 @@ const option = {
 }
 
 const caver = new Caver(new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", option)); //누구한테 가서 실행할지
-const CountContract = new caver.contract(COUNT_ABI, COUNT_COUNTRACT_ADRESS); //컨트렉트 주소와 abi를 넣어줌.
+const CountContract = new caver.contract(COUNT_ABI, COUNT_CONTRACT_ADDRESS); //컨트렉트 주소와 abi를 넣어줌.
 
 const readCount = () => { // contract의 count 메서드를 실행시키는 함수
     const _count = CountContract.methods.count().call(); // 주소에 가서 count를 실행해줘!
