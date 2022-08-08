7898ddb7e40f314cd5cd85824b49f5a78947483e
kevin <kevin0181@naver.com>
2022-08-08 15:58:28 +0900

    chap4 4.caver-js(write): create setCount function & get change count success

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Caver from "caver-js";
 
 //컨트렉트 주소 0x8983dA5d7f24Cb0314332c038eC6Ee8c1DFF552b (Count.sol)
 
-const COUNT_COUNTRACT_ADRESS = '0x8983dA5d7f24Cb0314332c038eC6Ee8c1DFF552b'; //contract address
+const COUNT_COUNTRACT_ADRESS = '0x1e8c426B58F6c26BD9A9Bf4E988F7371B87af110'; //contract address
 const ACCESS_KEY_ID = "KASKKBBA9XK2CNVHJRXNIVBY" //kas access key
 const SECRET_ACCESS_KEY = "q72Y3eh-qIcu-YmylbIvIOu27VNmJzFXgduUg2eJ"; //kas secret access key
 
@@ -54,17 +54,37 @@ const option = {
 
 const caver = new Caver(new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", option)); //누구한테 가서 실행할지
 const CountContract = new caver.contract(COUNT_ABI, COUNT_COUNTRACT_ADRESS); //컨트렉트 주소와 abi를 넣어줌.
-const readCount = () => {
+
+const readCount = () => { // contract의 count 메서드를 실행시키는 함수
     const _count = CountContract.methods.count().call(); // 주소에 가서 count를 실행해줘!
     console.log(_count);
 }
 
-const getBalance = (address) => {
+const getBalance = (address) => { // 주소에 klay가 얼마나 들어있는지 확인하는 함수
     return caver.rpc.klay.getBalance(address).then((res) => {
         const balance = caver.utils.convertFromPeb(caver.utils.hexToNumberString(res));
         console.log('balance : ' + balance);
         return balance;
     })
+}
+
+const setCount = async (newCount) => {
+    // 사용할 account 설정 key를 통해 지갑설정.
+    try {
+        const privateKey = "0xd6c51612b9cf9c6c439ae502f892eb18a2b0c5187c623e24743728844d52f4d0"; //여기다가 이렇게 프라이빗키 적으면안댐. 원래는.
+        const deployer = caver.wallet.keyring.createFromPrivateKey(privateKey);
+        caver.wallet.add(deployer);
+        // 스마트 컨트렉트 실행 트랜잭션 날리기
+        // 결과 콘솔로 찍어내기
+        const receipt = await CountContract.methods.setCount(newCount).send({
+            from: deployer.address,
+            gas: "0x4bfd200", // 아무거나 작성해도 되는데, 어차피 setCount 함수를 실행하는 만큼만 가스비가 지출이 된다.   근데 아무거나 라는게 진짜아무거나 작성해도되려나..?
+        });
+        console.log(receipt);
+    } catch (e) {
+        console.log(e);
+    }
+
 }
 
 function App() {
@@ -74,6 +94,10 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
+                <button title={'카운트 변경'} onClick={() => {
+                    setCount(100)
+                }}>카운트 변경
+                </button>
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
