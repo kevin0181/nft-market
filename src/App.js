6270b58ec570c405792272cf420d81a25a6d41fb
kevin <kevin0181@naver.com>
2022-08-09 11:02:02 +0900

    chap4 6. klip2: 일단 기본적으로 klip api를 사용해서 qr코드를 사용해 klip 주소를 가져오고, qr코드로 사용해서 setCount 함수를 이용해 값을 변경함

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 import {readCount, getBalance, setCount} from "./api/UseCaver";
+import QRCode from "qrcode.react";
+import {useState} from "react";
+import * as KlipAPI from "./api/UseKlip";
 
+
+const DEFAULT_QR_CODE = "DEFAULT";
 
 function App() {
-    readCount();
-    getBalance("0x27b0e5b4d3f72e8f26f4c58a5ee7b04516fb3034");
+
+    // readCount();
+    // getBalance("0x27b0e5b4d3f72e8f26f4c58a5ee7b04516fb3034");
+
+    const [qrValue, setQrValue] = useState(DEFAULT_QR_CODE);
+
+    const onClickGetAddress = () => { // 클릭시 klip 지갑의 주소를 가져옴
+        KlipAPI.getAddress(setQrValue);
+    }
+
+    const onClickSetCount = () => {
+        KlipAPI.setCount(2000, setQrValue);
+    }
+
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <button title={'카운트 변경'} onClick={() => {
-                    setCount(202)
-                }}>카운트 변경
+                <button onClick={() => {
+                    onClickGetAddress();
+                }}>주소 가져오기
                 </button>
+                <button onClick={() => {
+                    onClickSetCount();
+                }}>카운트값 변경
+                </button>
+                <br/>
+                <br/>
+                <br/>
+                <QRCode value={qrValue}/>
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a
                     className="App-link"
---
+++ src/api/UseKlip.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import {COUNT_CONTRACT_ADDRESS} from "../constants";
+
+
+const A2A_API_PREPARE_URL = 'https://a2a-api.klipwallet.com/v2/a2a/prepare'
+const APP_NAME = "KLAY_MARKET";
+
+export const setCount = (count, setQrValue) => {
+
+    axios.post(A2A_API_PREPARE_URL, {
+        bapp: {
+            name: APP_NAME
+        },
+        type: "execute_contract",
+        transaction: {
+            to: COUNT_CONTRACT_ADDRESS,
+            abi: `{ "constant": false, "inputs": [ { "name": "_count", "type": "uint256" } ], "name": "setCount", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }`,
+            value: "0",
+            params: `[\"${count}\"]`
+        }
+    }).then(res => {
+        const {request_key} = res.data;
+
+        const qrCode = `https://klipwallet.com/?target=/a2a?request_key=${request_key}`;
+        setQrValue(qrCode);
+        let timerId = setInterval(() => {
+            axios.get(`https://a2a-api.klipwallet.com/v2/a2a/result?request_key=${request_key}`).then(res => {
+                if (res.data.result) {
+                    console.log(res.data.result);
+                    clearInterval(timerId);
+                }
+            })
+        }, 1000)
+    })
+
+}
+
+export const getAddress = (setQrValue) => {
+
+    axios.post('https://a2a-api.klipwallet.com/v2/a2a/prepare', {
+        bapp: {
+            name: 'KLAY_MARKET'
+        },
+        type: "auth",
+    }).then(res => {
+        const {request_key} = res.data;
+
+        const qrCode = `https://klipwallet.com/?target=/a2a?request_key=${request_key}`;
+        setQrValue(qrCode);
+        let timerId = setInterval(() => {
+            axios.get(`https://a2a-api.klipwallet.com/v2/a2a/result?request_key=${request_key}`).then(res => {
+                if (res.data.result) {
+                    console.log(res.data.result);
+                    clearInterval(timerId);
+                }
+            })
+        }, 1000)
+    })
+
+}
