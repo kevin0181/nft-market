1baf2af6f0389025921934739e30cb07c3a77d34
kevin <kevin0181@naver.com>
2022-08-23 18:11:23 +0900

    chap5-5 완료

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import QRCode from "qrcode.react";
 import {useEffect, useState} from "react";
 import * as KlipAPI from "./api/UseKlip";
 import "bootstrap/dist/css/bootstrap.css"
-import {Alert, CardImg, Card, Container, Image} from "react-bootstrap";
+import {Alert, CardImg, Card, Container, Image, Nav, FormGroup, FormControl, Button, Form} from "react-bootstrap";
 import {MARKET_CONTRACT_ADDRESS} from "./constants";
+import {mintCardWithURI} from "./api/UseKlip";
 
 
 const DEFAULT_QR_CODE = "DEFAULT";
@@ -22,15 +23,9 @@ function App() {
     const [myAddress, setMyAddress] = useState(DEFAULT_ADDRESS);
 
     const [qrValue, setQrValue] = useState(DEFAULT_QR_CODE);
+    const [tab, setTab] = useState('MARKET');
 
-    useEffect(() => {
-        fetchMyNFTs();
-    }, [])
-
-    useEffect(() => {
-        console.log(nfts);
-    }, [nfts]);
-
+    const [mintImageUrl, setMintImageUrl] = useState("");
 
     /**
      * 마켓의 NFT들을 가지고옴
@@ -64,6 +59,18 @@ function App() {
         });
     }
 
+    /**
+     * NFT 발행
+     */
+    const onClickMint = (uri) => {
+        if (myAddress === DEFAULT_ADDRESS)
+            alert("NO ADDRESS");
+        const randomTokenId = parseInt(Math.random() * 10000000);
+        mintCardWithURI(myAddress, randomTokenId, uri, setQrValue, (result) => {
+            alert(JSON.stringify(result));
+        })
+    }
+
     return (
         <div className="App">
             <div style={{backgroundColor: "black", padding: 10}}>
@@ -75,16 +82,94 @@ function App() {
                     variant={"balance"} style={{backgroundColor: "#f40075", fontSize: 25}}>
                     {myBalance}
                 </Alert>
-                <div className={"container"} style={{padding: 0, width: "100%"}}>
-                    {nfts.map((nft, index) => (
-                        <CardImg key={index} className={"img-responsive"} src={nft.uri}/>
-                    ))}
-                </div>
+
+                <Container style={{backgroundColor: 'white', width: 300, height: 300, padding: 20}}>
+                    <QRCode value={qrValue} size={256} style={{margin: "auto"}}/>
+                </Container>
+
+                {
+                    tab === "MARKET" || tab === "WALLET" ? (
+                        <div className={"container"} style={{padding: 0, width: "100%"}}>
+                            {nfts.map((nft, index) => (
+                                <CardImg key={index} className={"img-responsive"} src={nft.uri}/>
+                            ))}
+                        </div>
+
+                    ) : (
+                        <></>
+                    )
+                }
+
+                {
+                    tab === "MINT" ? (
+                        <div className={"container"} style={{padding: 0, width: "100%"}}>
+                            <Card className={"text-center"}
+                                  style={{color: "black", height: "50%", borderColor: "#C5B358"}}>
+                                <Card.Body style={{opacity: 0.9, backgroundColor: "black"}}>
+                                    {
+                                        mintImageUrl !== "" ?
+                                            <CardImg src={mintImageUrl} height={"50%"}/> : null
+                                    }
+                                    <Form>
+                                        <FormGroup>
+                                            <FormControl
+                                                value={mintImageUrl}
+                                                onChange={(e) => {
+                                                    setMintImageUrl(e.target.value);
+                                                }}
+                                                type={"text"}
+                                                placeholder={"이미지 주소를 입력해주세요."}
+                                            />
+                                        </FormGroup>
+                                        <br/>
+                                        <Button
+                                            onClick={() => {
+                                                onClickMint(mintImageUrl);
+                                            }}
+                                            variant={"primary"} style={{
+                                            backgroundColor: "#810034",
+                                            borderColor: "#810034"
+                                        }}>발행하기</Button>
+                                    </Form>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    ) : (
+                        <></>
+                    )
+                }
 
             </div>
-            <Container style={{backgroundColor: 'white', width: 300, height: 300, padding: 20}}>
-                <QRCode value={qrValue} size={256} style={{margin: "auto"}}/>
-            </Container>
+            <nav style={{backgroundColor: "#1b1717", height: 45}} className={"navbar fixed-bottom navbar-light"}
+                 role={"navigation"}>
+                <Nav className={"w-100"}>
+                    <div className={"d-flex flex-row justify-content-around w-100"}>
+                        <div onClick={() => {
+                            setTab("MARKET");
+                            fetchMarketNFTs();
+                        }} className={"row d-flex flex-column justify-content-center align-items-center"}>
+                            <div>
+                                MARKET
+                            </div>
+                        </div>
+                        <div onClick={() => {
+                            setTab("MINT");
+                        }} className={"row d-flex flex-column justify-content-center align-items-center"}>
+                            <div>
+                                MINT
+                            </div>
+                        </div>
+                        <div onClick={() => {
+                            setTab("WALLET");
+                            fetchMyNFTs();
+                        }} className={"row d-flex flex-column justify-content-center align-items-center"}>
+                            <div>
+                                WALLET
+                            </div>
+                        </div>
+                    </div>
+                </Nav>
+            </nav>
         </div>
     );
 }
--- src/api/UseKlip.js
+++ src/api/UseKlip.js
@@ -1,41 +1,41 @@
 import axios from "axios";
-import {COUNT_CONTRACT_ADDRESS} from "../constants";
+import {COUNT_CONTRACT_ADDRESS, NFT_CONTRACT_ADDRESS} from "../constants";
 
 
 const A2A_API_PREPARE_URL = 'https://a2a-api.klipwallet.com/v2/a2a/prepare'
 const APP_NAME = "KLAY_MARKET";
 
-export const setCount = (count, setQrValue) => {
-
-    axios.post(A2A_API_PREPARE_URL, {
-        bapp: {
-            name: APP_NAME
-        },
-        type: "execute_contract",
-        transaction: {
-            to: COUNT_CONTRACT_ADDRESS,
-            abi: `{ "constant": false, "inputs": [ { "name": "_count", "type": "uint256" } ], "name": "setCount", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }`,
-            value: "0",
-            params: `[\"${count}\"]`
-        }
-    }).then(res => {
-        const {request_key} = res.data;
-
-        const qrCode = `https://klipwallet.com/?target=/a2a?request_key=${request_key}`;
-        setQrValue(qrCode);
-        let timerId = setInterval(() => {
-            axios.get(`https://a2a-api.klipwallet.com/v2/a2a/result?request_key=${request_key}`).then(res => {
-                if (res.data.result) {
-                    console.log(res.data.result);
-                    if (res.data.result.status === "success") {
-                        clearInterval(timerId);
-                    }
-                }
-            })
-        }, 1000)
-    })
-
-}
+// export const setCount = (count, setQrValue) => {
+//
+//     axios.post(A2A_API_PREPARE_URL, {
+//         bapp: {
+//             name: APP_NAME
+//         },
+//         type: "execute_contract",
+//         transaction: {
+//             to: COUNT_CONTRACT_ADDRESS,
+//             abi: `{ "constant": false, "inputs": [ { "name": "_count", "type": "uint256" } ], "name": "setCount", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }`,
+//             value: "0",
+//             params: `[\"${count}\"]`
+//         }
+//     }).then(res => {
+//         const {request_key} = res.data;
+//
+//         const qrCode = `https://klipwallet.com/?target=/a2a?request_key=${request_key}`;
+//         setQrValue(qrCode);
+//         let timerId = setInterval(() => {
+//             axios.get(`https://a2a-api.klipwallet.com/v2/a2a/result?request_key=${request_key}`).then(res => {
+//                 if (res.data.result) {
+//                     console.log(res.data.result);
+//                     if (res.data.result.status === "success") {
+//                         clearInterval(timerId);
+//                     }
+//                 }
+//             })
+//         }, 1000)
+//     })
+//
+// }
 
 export const getAddress = (setQrValue, callback) => {
 
@@ -60,4 +60,40 @@ export const getAddress = (setQrValue, callback) => {
         }, 1000)
     })
 
+}
+
+export const mintCardWithURI = async (toAddress, tokenId, uri, setQrValue, callback) => {
+    const functionJson = '{"constant": false, "inputs": [ { "name": "to", "type": "address" }, { "name": "tokenId", "type": "uint256" }, { "name": "tokenURI", "type": "string" } ], "name": "mintWithTokenURI", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }';
+    executeContract(NFT_CONTRACT_ADDRESS, functionJson, "0", `[\"${toAddress}\",\"${tokenId}\",\"${uri}\"]`, setQrValue, callback);
+}
+
+export const executeContract = (txTo, functionJSON, value, params, setQrValue, callback) => {
+    axios.post(A2A_API_PREPARE_URL, {
+        bapp: {
+            name: APP_NAME
+        },
+        type: "execute_contract",
+        transaction: {
+            to: txTo,
+            abi: functionJSON,
+            value: value,
+            params: params
+        }
+    }).then(res => {
+        const {request_key} = res.data;
+
+        const qrCode = `https://klipwallet.com/?target=/a2a?request_key=${request_key}`;
+        setQrValue(qrCode);
+        let timerId = setInterval(() => {
+            axios.get(`https://a2a-api.klipwallet.com/v2/a2a/result?request_key=${request_key}`).then(res => {
+                if (res.data.result) {
+                    console.log(res.data.result);
+                    if (res.data.result.status === "success") {
+                        callback(res.data.result);
+                        clearInterval(timerId);
+                    }
+                }
+            })
+        }, 1000)
+    })
 }
